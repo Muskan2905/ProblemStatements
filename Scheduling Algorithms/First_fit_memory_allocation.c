78401c9f57977f32482e41f59eb4f6ccf58afe9f
Muskan2905 <69806508+Muskan2905@users.noreply.github.com>
2021-07-05 18:53:23 +0530

    Update First_fit_memory_allocation.c

--- Scheduling Algorithms/First_fit_memory_allocation.c
+++ Scheduling Algorithms/First_fit_memory_allocation.c
@@ -4,34 +4,32 @@
 #include<conio.h>
 
 int main(){
-int block_size[20],process_size[20],memory_allocated[20],process_allocated[20],b , p;
+int block_size[20],process_size[20],memory_allocated[20],process_allocated[20],b,p,i,j;
 printf("Enter the no. of memory blocks\n");
 scanf("%d",&b);
 printf("Enter the size of each memory block\n");
-for(int i = 0;i<b;i++){
+for(i = 0;i<b;i++){
     scanf("%d",&block_size[i]);
     memory_allocated[i] = 0;
 }
 printf("Enter the no. of processes\n");
 scanf("%d",&p);
 printf("Enter the size of each process\n");
-for(int i = 0;i<p;i++){
+for(i = 0;i<p;i++){
     printf("Process P%d = ",i+1);
     scanf("%d",&process_size[i]);
     process_allocated[i] = 0;
 }
-for(int i = 0;i<p;i++){
-    for(int j = 0;j<b;j++){
+for(i = 0;i<p;i++){
+    for(j = 0;j<b;j++){
         if((memory_allocated[j] == 0) && (block_size[j] >= process_size[i])){
             process_allocated[i] = 1;
             memory_allocated[j] = 1;
             break;
         }
     }
-}
-for(int i = 0;i<p;i++){
     if(process_allocated[i] == 1){
-        printf("Process P%d is allocated\n",i+1);
+        printf("Process P%d of size %d is allocated at position %d of memory block of size %d\n",i+1,process_size[i],j+1,block_size[j]);
     }
     else{
         printf("Process P%d is not allocated\n",i+1);
